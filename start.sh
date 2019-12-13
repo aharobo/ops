e1393e331572cb939ef51ec366d6ae5705968934
Joe <44820005@qq.com>
2019-12-13 17:14:16 +0800

    i

--- start.sh
+++ start.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 clear
-python /root/ops/updomain.py startup
-echo "HELLO PYTHON"
+
+alias rc='sudo raspi-config'
+alias cpu='temp=$(cat /sys/class/thermal/thermal_zone0/temp); echo "Current CPU TEMP:${temp: 0:-3} C$
+alias lh='ls -l -h'
+
+
+sleep  3
+cpu
+python3 /var/ops/ip_up_push.py auto true aoto_login
+
+
+echo "HELLO BOY"
